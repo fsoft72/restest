24cc6228a9fb99cf679235a717a88f44f94b2693
Fabio Rotondo <fabio.rotondo@gmail.com>
2020-08-25 17:30:25 +0200

    Support for Headers and Auth remapping

--- lib/postman_exp.py
+++ lib/postman_exp.py
@@ -4,7 +4,7 @@ import os, json
 from urllib.parse import urlparse
 
 class PostmanExporterItem:
-	def __init__ ( self, title, mode, url, data, base_url ):
+	def __init__ ( self, title, mode, url, data, base_url, headers, auth_name, auth_value ):
 		self.mode = mode
 		self.name = None
 		self.url = {}
@@ -12,6 +12,9 @@ class PostmanExporterItem:
 		self.title = title
 		self.body = None
 		self.base_url = base_url
+		self.headers = headers
+		self.auth_name = auth_name
+		self.auth_value = auth_value
 
 		self._parse ( url )
 
@@ -34,6 +37,7 @@ class PostmanExporterItem:
 		}
 
 		self._parse_body ()
+		self._parse_headers ()
 
 	def _parse_body ( self ):
 		if not len ( self.data.keys () ): return
@@ -50,12 +54,27 @@ class PostmanExporterItem:
 
 		self.body = body
 
+	def _parse_headers ( self ):
+		if not len ( self.headers ): return
+		print ( "\n\n\n***************** HEADERS **************************\n\n", self.headers )
+		res = []
+		for k, v in self.headers.items ():
+			if k == self.auth_name: v = self.auth_value
+
+			res.append ( {
+				"key": k,
+				"value": v,
+				"type": "text"
+			} )
+
+		self.headers = res
+
 	def obj ( self ):
 		res = {
 			"name": self.name,
 			"request": {
 				"method": self.mode,
-				"header": [],
+				"header": self.headers,
 				"url": self.url,
 				"description": self.title
 			},
@@ -65,21 +84,25 @@ class PostmanExporterItem:
 		if self.body:
 			res [ 'request' ] [ 'body' ] = self.body
 
+
 		return res
 
 class PostmanExporter:
-	def __init__ ( self, output_name, name, base_url ):
+	def __init__ ( self, output_name, name, base_url, auth_name, auth_value ):
 		self.schema = "https://schema.getpostman.com/json/collection/v2.1.0/collection.json"
 		self.output_name = output_name
 		self.name = name
 		self.base_url = base_url
+		self.auth_name = auth_name
+		self.auth_value = auth_value
+
 		if not self.name:
 			self.name = os.path.basename ( output_name ).split ( "." ) [ 0 ]
 
 		self.items = []
 
 	def add ( self, title, mode, url, data, headers, r ):
-		it = PostmanExporterItem ( title, mode, url, data, self.base_url )
+		it = PostmanExporterItem ( title, mode, url, data, self.base_url, headers, self.auth_name, self.auth_value )
 		self.items.append ( it )
 
 	def save ( self ):
