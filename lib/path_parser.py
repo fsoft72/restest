8ae697c8bb2bb03d4688b7d0b4af5226bc446918
Fabio Rotondo <fabio.rotondo@gmail.com>
2021-02-02 08:03:27 +0100

    Path Parser working and supporting recursion

--- lib/path_parser.py
+++ lib/path_parser.py
@@ -5,7 +5,6 @@ import re
 rx_split = re.compile ( r"([.!=[\]])" )
 
 def _is_int ( txt ):
-	print ( "--- TXT: ", txt )
 	try:
 		num = int ( str ( txt ), 10 )
 		return True
@@ -14,13 +13,25 @@ def _is_int ( txt ):
 
 def _parse_square ( tokens, pos ):
 	res = []
+	tok = tokens [ pos ]
 
-	while pos < len ( tokens ):
-		tok = tokens [ pos ]
-		pos += 1
+	if _is_int ( tok ):
+		return { "mode": "pos", "value": int ( str ( tok ), 10 ) }, pos +2
 
-		if _is_int ( tok ):
-			return { "mode": "pos", "value": int ( str ( tok ), 10 ) }, pos
+	return _parser ( tokens, pos )
+
+def _extract_value ( tokens, pos, equal = False ):
+	mode = "equal"
+	if not equal:
+		mode = "not_equal"
+		pos = pos + 1
+
+	tok = tokens [ pos ]
+
+	pos = pos + 1
+
+	return { "mode": mode, "value": tok }, pos
+
 
 def _parser ( tokens, pos ):
 	res = []
@@ -34,18 +45,29 @@ def _parser ( tokens, pos ):
 			res.append ( { "mode": "label", "value": tok } )
 		elif tok == '[':
 			parsed, pos = _parse_square ( tokens, pos )
-
 			res.append ( parsed )
+		elif tok == ']':
+			return res, pos
+		elif tok == '=':
+			parsed, pos = _extract_value ( tokens, pos, equal = True )
+			res.append ( parsed )
+		elif tok == '!':
+			parsed, pos = _extract_value ( tokens, pos, equal = False )
+			res.append ( parsed )
+
 	return res, pos
 
 def path_parser ( path ):
 	print ( "PATH: ", path )
 	tokens = rx_split.split ( path )
 
-	return _parser ( tokens, 0 )
+	return _parser ( tokens, 0 ) [ 0 ]
 
 
 if __name__ == '__main__':
-	print ( path_parser ( "first.second.[0].third" ) )
-	print ( path_parser ( "first.second.[user.key=5].third" ) )
-	print ( path_parser ( "first.second.[user.email!=example@gmail.com].third" ) )
+	import json
+	#print ( path_parser ( "first.second.[0].third" ) )
+	#print ( json.dumps ( path_parser ( "first.second.[user.key=5].third" ), indent = 4 ) )
+	#print ( path_parser ( "first.second.[user.email!=example@gmail.com].third" ) )
+	#print ( json.dumps ( path_parser ( "first.[3].[user.key=23].second.[1].name" ), indent = 4 ) )
+	print ( json.dumps ( path_parser ( "first.[sub[user.key=3]].second" ), indent=4 ) )
