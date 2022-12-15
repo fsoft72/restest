b84e62b3369976b29d825fd7759961e267f125d2
fsoft <fabio.rotondo@gmail.com>
2022-12-15 17:30:51 +0100

    Better path_parser capabilities with quotes support

--- lib/path_parser.py
+++ lib/path_parser.py
@@ -2,7 +2,11 @@
 
 import re
 
-rx_split = re.compile(r"([.!=[\]])")
+# s = "[id='course.080ca4b4e3cb4e1e1b6b99776e8fb621.3tk0']"
+# a regex that splits s on "[", "]", "=", ".", "!" and keeps the content of single and double quotes in one piece
+# so that the result is: ["[", "id", "=", "'course.080ca4b4e3cb4e1e1b6b99776e8fb621.3tk0'", "]"]
+
+rx_split = re.compile(r"""(\[|\]|=|!|\.|'.*?'|".*?")""")
 
 
 def _is_int(txt):
@@ -29,6 +33,10 @@ def _extract_value(tokens, pos, equal=False):
         pos = pos + 1
 
     tok = tokens[pos]
+
+    # if tok starts with a quote or double quote, then we remove them
+    if tok[0] in ("'", '"'):
+        tok = tok[1:-1]
 
     pos = pos + 1
 
@@ -65,7 +73,7 @@ def _parser(tokens, pos, is_pattern=False):
 
 
 def path_parser(path):
-    tokens = rx_split.split(path)
+    tokens = [t for t in rx_split.split(path) if t]
 
     return _parser(tokens, 0)[0]
 
@@ -145,66 +153,66 @@ def expand_value(path, dct):
 
 
 if __name__ == "__main__":
-    import json
-
     # print ( path_parser ( "first.second.[0].third" ) )
     # print ( json.dumps ( path_parser ( "first.second.[user.key=5].third" ), indent = 4 ) )
     # print ( path_parser ( "first.second.[user.email!=example@gmail.com].third" ) )
     # print ( json.dumps ( path_parser ( "first.[3].[user.key=23].second.[1].name" ), indent = 4 ) )
     # print ( json.dumps ( path_parser ( "first.[sub[user.key=3]].second" ), indent=4 ) )
-    x = """
-    {
+    x = {
         "type1": [
-            {
-                "name": "hello",
-                "value": "world"
-            },
-            {
-                "name": "ciao",
-                "value": "mondo"
-            }
+            {"name": "hello", "value": "world"},
+            {"name": "ciao", "value": "mondo"},
         ],
         "users": [
             {
                 "email": "test01@example.com",
                 "id": 123,
-                "perms": [
-                    "admin",
-                    "work",
-                    "home"
-                ]
+                "perms": ["admin", "work", "home"],
             },
-            {
-                "email": "test02@example.com",
-                "id": 999,
-                "perms": [
-                    "do",
-                    "this"
-                ]
-            }
+            {"email": "test02@example.com", "id": 999, "perms": ["do", "this"]},
         ],
         "nested": {
             "opts": [
                 {
                     "name": "opt1",
                     "children": [
-                        {
-                            "name": "child1",
-                            "value": 1
-                        },
-                        {
-                            "name": "child2",
-                            "value": 2
-                        }
-                    ]
+                        {"name": "child1", "value": 1},
+                        {"name": "child2", "value": 2},
+                    ],
                 }
             ]
-        }
+        },
     }
-    """
-    dct = json.loads(x)
+    dct = {
+        "courses": [
+            {
+                "id": "course.080ca4b4e3cb4e1e1b6b99776e8fb621.3tk0",
+                "slug": "course-test",
+                "code": "course001",
+                "id_owner": "user.8edb2cf662f639dcd3ac9e0c0c5a1816.bq2n",
+                "visible": True,
+                "tot_sections": 1,
+                "tot_time": 600,
+                "tot_lessons": 3,
+                "type": "default",
+                "max_students": 0,
+                "num_students": 0,
+                "revenue_platform": 30,
+                "revenue_trainer": 70,
+                "title": "Course Test",
+                "id_product": "product.7aa9e1a1a3d39b7d09bd9d71f4e3d09d.ek75",
+                "id_trainers": ["user.8edb2cf662f639dcd3ac9e0c0c5a1816.bq2n"],
+                "description": "This is the updated description",
+                "tags": [],
+            }
+        ]
+    }
+    # dct = json.loads(x)
     # print ( expand_value ( "type1.[0].name", dct ) )
     # print ( expand_value ( "type1.[0].value", dct ) )
-    # print ( expand_value ( "type1.[name=ciao].value", dct ) )
+    # print(expand_value("type1.[name=ciao].value", dct))
     # print ( expand_value ( "users.[id=123].perms", dct ) )
-    print(expand_value("nested.opts[name=opt1].children[name!=child2].value", dct))
+    # print(expand_value("nested.opts[name=opt1].children[name!=child2].value", dct))
+    print(
+        expand_value("courses.[id='course.080ca4b4e3cb4e1e1b6b99776e8fb621.3tk0']", dct)
+    )
