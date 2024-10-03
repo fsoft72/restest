208c5280d573dee277ebfcdac6f5ea79f5a12ea7
Fabio Rotondo <fabio.rotondo@gmail.com>
2024-10-03 17:45:50 +0200

    ADD: size2str()

--- lib/utils.py
+++ lib/utils.py
@@ -5,3 +5,15 @@ import copy
 
 def deepcopy(o):
     return copy.deepcopy(o)
+
+
+def size2str(size):
+    size = int(size)
+    if size < 1024:
+        return str(size) + "B"
+    elif size < 1024 * 1024:
+        return str(size // 1024) + "KB"
+    elif size < 1024 * 1024 * 1024:
+        return str(size // 1024 // 1024) + "MB"
+    else:
+        return str(size // 1024 // 1024 // 1024) + "GB"
