39bcbe111b0c343c48bf1ad98b9f82864c6b429b
Fabio Rotondo <fabio.rotondo@gmail.com>
2025-07-17 11:41:36 +0200

    Now package version in setup is read from restest.py/VERSION variable

--- setup.py
+++ setup.py
@@ -7,9 +7,26 @@ from pathlib import Path
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
+# Read the VERSION line from restest.py
+version_file = this_directory / "restest.py"
+with open(version_file, "r") as f:
+    for line in f:
+        if line.startswith("VERSION ="):
+            VERSION = line.split('"')[1]  # Extract the version number
+            break
+
+print("=== VERSION: %s ===" % VERSION)
+
+if VERSION.endswith("-dev"):
+    print(
+        "ERROR: You are installing a development version of restest. "
+        "This version may not be stable and could contain bugs."
+    )
+    exit(1)
+
 setup(
     name="restest",
-    version="2.3.0",
+    version=VERSION,
     description="Scriptable REST calls test software written in Python",
     long_description=long_description,
     long_description_content_type="text/markdown",
