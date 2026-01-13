deeac1f47e9c8ca14cb57d25b31ad46ce6c3599b
Fabio Rotondo <fabio.rotondo@gmail.com>
2026-01-13 12:43:56 +0100

    ENHANCE: display included file content in include action
    - Show very visible output when including files
    - Cyan borders (80 chars) with bold header on blue background
    - Filename in bold yellow
    - Does not show full JSON content (only filename)
    - Helps users see which files are being loaded

--- lib/parser.py
+++ lib/parser.py
@@ -384,21 +384,16 @@ class RESTestParser:
 
         script = self._json_load(fname)
 
-        # 2.5.0 - Display included file content in console
+        # 2.5.0 - Display included filename in console
         print(
-            "\n%s\n%s %s\n%s"
+            "\n%s\n%s %s\n%s\n"
             % (
                 xcolored(self, "=" * 80, "cyan"),
                 xcolored(self, "INCLUDING FILE:", "white", "on_blue", ["bold"]),
                 xcolored(self, fname, "yellow", attrs=["bold"]),
-                xcolored(self, "-" * 80, "cyan"),
+                xcolored(self, "=" * 80, "cyan"),
             )
         )
-        # Pretty print the JSON content
-        formatted_json = json.dumps(script, indent=2, default=str)
-        for line in formatted_json.split("\n"):
-            print(xcolored(self, line, "green"))
-        print(xcolored(self, "=" * 80, "cyan") + "\n")
 
         skip_include = False
         if script.get("run-once", False) and fname in self._included:
